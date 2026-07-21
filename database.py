690b2a780dc49a10ef88088a5c49316bff47de06
aryanxcheatmodz <touhidalam159@gmail.com>
2026-07-22 05:20:32 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from config import *
 
+
 def connect():
     return mysql.connector.connect(
         host=DATABASE_HOST,
@@ -8,3 +9,31 @@ def connect():
         password=DATABASE_PASSWORD,
         database=DATABASE_NAME
     )
+
+
+def create_tables():
+
+    db = connect()
+    cursor = db.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(100),
+        password VARCHAR(100),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS keys_table(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        license_key VARCHAR(100),
+        expiry_date DATE,
+        status VARCHAR(20)
+    )
+    """)
+
+    db.commit()
+    db.close()
